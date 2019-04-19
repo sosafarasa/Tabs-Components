1b51546d79f43e24319b84dc2f8c0f65a402aa08
Agustina Sosa <agustinasf85@gmail.com>
2019-04-19 00:44:49 -0400

    working on tabs.js, not done

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,27 +2,33 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
+    this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector(`.itemElement[data-tab='${this.data}']`);
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = new TabItem(this.item);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => {
+      this.select();
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
+
+    links.forEach( link => {
+      link.classList.remove('tabs-link-selected')
+    });
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
@@ -53,10 +59,14 @@ class TabItem {
 
 - Select all classes named ".tabs-link" and assign that value to the links variable
 
-- With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
+- With your selection in place, now chain a .forEach() method onto the links variable to 
+iterate over the DOM NodeList
 
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
+- In your .forEach() method's callback function, return a new instance of TabLink and 
+pass in each link as a parameter
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll(".tabs-link").forEach( tabs-link => {
+  return new TabLink(tabs-link);
+});
